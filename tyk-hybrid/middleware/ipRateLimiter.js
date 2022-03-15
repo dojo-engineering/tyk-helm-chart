4086ab54a2ec3f2d570d768b727727229be223e7
George Grasham <George.Grasham@paymentsense.com>
2022-03-15 16:43:21 +0000

    fix ip rate limit middleware

--- tyk-hybrid/middleware/ipRateLimiter.js
+++ tyk-hybrid/middleware/ipRateLimiter.js
@@ -1,32 +1,34 @@
     // ---- A Middleware based IP Rate limiter -----
     var ipRateLimiter = new TykJS.TykMiddleware.NewMiddleware({});
 
-    ipRateLimiter.NewProcessRequest(function(request) {
+    ipRateLimiter.NewProcessRequest(function(request, session, spec) {
         // Get the IP address
-        var thisIP = request.Headers["X-Real-Ip"][0];
+        var thisIPChain = request.Headers["X-Forwarded-For"][0];
+        var ips = thisIPChain.split(",")
+        var thisIP = ips[ips.length - 2]
     
         // Set auth header
         request.SetHeaders["x-tyk-authorization"] = thisIP;
     
+        var thisSession = JSON.parse(TykGetKeyData(thisIP ,spec.APIID))
+        log(JSON.stringify(thisSession))
+
         var keyDetails = {
-            "allowance": 100,
-            "rate": 100,
-            "per": 1,
+            "allowance": 5,
+            "rate": 5,
+            "per": 5,
             "expires": 0,
-            "quota_max": 100,
-            "quota_renews": 1406121006,
-            "quota_remaining": 100,
-            "quota_renewal_rate": 60,
-            "access_rights": {
-                "ip-ratelimit-api": {
-                    "api_name": "Test API",
-                    "api_id": "ip-ratelimit-api",
-                    "versions": [
-                        "Default"
-                    ]
-                }
-            },
-            "org_id": "620e2a010200200001a77b3f"
+            "quota_max": -1,
+            "quota_renews": 0,
+            "quota_remaining": -1,
+            "quota_renewal_rate": 0,
+            "access_rights": {},
+            "apply_policies": spec.config_data.ipRateLimiter.policies,
+            "org_id": "62210785d0929d0001b113b1"
+        }
+        keyDetails.access_rights[spec.config_data.ipRateLimiter.api_id] = {
+            "api_id": spec.config_data.ipRateLimiter.api_id,
+            "versions": spec.config_data.ipRateLimiter.versions,
         }
     
         TykSetKeyData(thisIP, JSON.stringify(keyDetails), 1)
@@ -34,5 +36,5 @@
         return ipRateLimiter.ReturnData(request);
     });
     
-    // Ensure init with a post-declaration log message
-    log("IP rate limiter JS initialised");
+// Ensure init with a post-declaration log message
+log("IP rate limiter JS initialised");
