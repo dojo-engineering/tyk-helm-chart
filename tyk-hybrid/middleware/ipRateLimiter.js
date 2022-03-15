0623d4243f4be482b86371dc6b1c04424bfeb9a9
George Grasham <George.Grasham@paymentsense.com>
2022-03-15 17:20:34 +0000

    only create key if it does not exist

--- tyk-hybrid/middleware/ipRateLimiter.js
+++ tyk-hybrid/middleware/ipRateLimiter.js
@@ -13,26 +13,28 @@
         var thisSession = JSON.parse(TykGetKeyData(thisIP ,spec.APIID))
         log(JSON.stringify(thisSession))
 
-        var keyDetails = {
-            "allowance": 5,
-            "rate": 5,
-            "per": 5,
-            "expires": 0,
-            "quota_max": -1,
-            "quota_renews": 0,
-            "quota_remaining": -1,
-            "quota_renewal_rate": 0,
-            "access_rights": {},
-            "apply_policies": spec.config_data.ipRateLimiter.policies,
-            "org_id": "62210785d0929d0001b113b1"
+        if (thisSession.status != "error"){
+            var keyDetails = {
+                "allowance": 5,
+                "rate": 5,
+                "per": 5,
+                "expires": 0,
+                "quota_max": -1,
+                "quota_renews": 0,
+                "quota_remaining": -1,
+                "quota_renewal_rate": 0,
+                "access_rights": {},
+                "apply_policies": spec.config_data.ipRateLimiter.policies,
+                "org_id": "62210785d0929d0001b113b1"
+            }
+            keyDetails.access_rights[spec.config_data.ipRateLimiter.api_id] = {
+                "api_id": spec.config_data.ipRateLimiter.api_id,
+                "versions": spec.config_data.ipRateLimiter.versions,
+            }
+            
+            TykSetKeyData(thisIP, JSON.stringify(keyDetails), 1)
         }
-        keyDetails.access_rights[spec.config_data.ipRateLimiter.api_id] = {
-            "api_id": spec.config_data.ipRateLimiter.api_id,
-            "versions": spec.config_data.ipRateLimiter.versions,
-        }
-    
-        TykSetKeyData(thisIP, JSON.stringify(keyDetails), 1)
-    
+
         return ipRateLimiter.ReturnData(request);
     });
     
