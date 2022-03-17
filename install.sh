5d57486bb3e20df9850625c6a00ee94c49ed6bf9
Ananya Kumar Mallik <ananya.mallik@paymentsense.com>
2022-03-17 11:35:03 +0000

    debug

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
+sleep 3600
 gcloud auth activate-service-account platform-publisher@firefly-devops-2018.iam.gserviceaccount.com --key-file=${CI_SECRETS_PATH}/plat-publish.json
 
 gcloud container clusters get-credentials platform --project firefly-devops-2018 --zone europe-west2-a
